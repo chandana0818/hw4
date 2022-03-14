f59a48984d32ead53eece278501ad116491e432e
chandana0818 <chandana.nagandla@kellogg.northwestern.edu>
2022-03-14 10:42:11 +0000

    add

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,8 +1,7 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
-    #where({user_id: @current_user.id})
+    @places = Place.where({user_id: session[:user_id]})
     
   end
 
@@ -18,7 +17,7 @@ class PlacesController < ApplicationController
 
   def create
     @place = Place.new(params["place"])
-    #@place.user_id = @current_user.id
+    @place.user_id = @current_user.id
     @place.save
     redirect_to "/places"
   end
